24c8313624f34109efd21997a6d6c04395de8d1f
chandola <chandola@buffalo.edu>
2015-04-14 20:22:49 -0400

    Initial upload of programming assignment assignment 3 script

--- homeworks/script.py
+++ homeworks/script.py
@@ -2,177 +2,195 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.io import loadmat
 from math import sqrt
-import scipy.io
-import matplotlib.pyplot as plt
-import pickle
 
-def ldaLearn(X,y):
-    # Inputs
-    # X - a N x d matrix with each row corresponding to a training example
-    # y - a N x 1 column vector indicating the labels for each training example
-    #
-    # Outputs
-    # means - A d x k matrix containing learnt means for each of the k classes
-    # covmat - A single d x d learnt covariance matrix 
+def preprocess():
+    """ 
+     Input:
+     Although this function doesn't have any input, you are required to load
+     the MNIST data set from file 'mnist_all.mat'.
+
+     Output:
+     train_data: matrix of training set. Each row of train_data contains 
+       feature vector of a image
+     train_label: vector of label corresponding to each image in the training
+       set
+     validation_data: matrix of training set. Each row of validation_data 
+       contains feature vector of a image
+     validation_label: vector of label corresponding to each image in the 
+       training set
+     test_data: matrix of training set. Each row of test_data contains 
+       feature vector of a image
+     test_label: vector of label corresponding to each image in the testing
+       set
+
+     Some suggestions for preprocessing step:
+     - divide the original data set to training, validation and testing set
+           with corresponding labels
+     - convert original data set from integer to double by using double()
+           function
+     - normalize the data to [0, 1]
+     - feature selection
+    """
     
-    # IMPLEMENT THIS METHOD
+    mat = loadmat('mnist_all.mat'); #loads the MAT object as a Dictionary
     
-    return means,covmat
-
-def qdaLearn(X,y):
-    # Inputs
-    # X - a N x d matrix with each row corresponding to a training example
-    # y - a N x 1 column vector indicating the labels for each training example
-    #
-    # Outputs
-    # means - A d x k matrix containing learnt means for each of the k classes
-    # covmats - A list of k d x d learnt covariance matrices for each of the k classes
+    n_feature = mat.get("train1").shape[1];
+    n_sample = 0;
+    for i in range(10):
+        n_sample = n_sample + mat.get("train"+str(i)).shape[0];
+    n_validation = 1000;
+    n_train = n_sample - 10*n_validation;
     
-    # IMPLEMENT THIS METHOD
+    # Construct validation data
+    validation_data = np.zeros((10*n_validation,n_feature));
+    for i in range(10):
+        validation_data[i*n_validation:(i+1)*n_validation,:] = mat.get("train"+str(i))[0:n_validation,:];
+        
+    # Construct validation label
+    validation_label = np.ones((10*n_validation,1));
+    for i in range(10):
+        validation_label[i*n_validation:(i+1)*n_validation,:] = i*np.ones((n_validation,1));
     
-    return means,covmats
-
-def ldaTest(means,covmat,Xtest,ytest):
-    # Inputs
-    # means, covmat - parameters of the LDA model
-    # Xtest - a N x d matrix with each row corresponding to a test example
-    # ytest - a N x 1 column vector indicating the labels for each test example
-    # Outputs
-    # acc - A scalar accuracy value
+    # Construct training data and label
+    train_data = np.zeros((n_train,n_feature));
+    train_label = np.zeros((n_train,1));
+    temp = 0;
+    for i in range(10):
+        size_i = mat.get("train"+str(i)).shape[0];
+        train_data[temp:temp+size_i-n_validation,:] = mat.get("train"+str(i))[n_validation:size_i,:];
+        train_label[temp:temp+size_i-n_validation,:] = i*np.ones((size_i-n_validation,1));
+        temp = temp+size_i-n_validation;
+        
+    # Construct test data and label
+    n_test = 0;
+    for i in range(10):
+        n_test = n_test + mat.get("test"+str(i)).shape[0];
+    test_data = np.zeros((n_test,n_feature));
+    test_label = np.zeros((n_test,1));
+    temp = 0;
+    for i in range(10):
+        size_i = mat.get("test"+str(i)).shape[0];
+        test_data[temp:temp+size_i,:] = mat.get("test"+str(i));
+        test_label[temp:temp+size_i,:] = i*np.ones((size_i,1));
+        temp = temp + size_i;
     
-    # IMPLEMENT THIS METHOD
-    return acc
+    # Delete features which don't provide any useful information for classifiers
+    sigma = np.std(train_data, axis = 0);
+    index = np.array([]);
+    for i in range(n_feature):
+        if(sigma[i] > 0.001):
+            index = np.append(index, [i]);
+    train_data = train_data[:,index.astype(int)];
+    validation_data = validation_data[:,index.astype(int)];
+    test_data = test_data[:,index.astype(int)];
 
-def qdaTest(means,covmats,Xtest,ytest):
-    # Inputs
-    # means, covmats - parameters of the QDA model
-    # Xtest - a N x d matrix with each row corresponding to a test example
-    # ytest - a N x 1 column vector indicating the labels for each test example
-    # Outputs
-    # acc - A scalar accuracy value
+    # Scale data to 0 and 1
+    train_data = train_data/255.0;
+    validation_data = validation_data/255.0;
+    test_data = test_data/255.0;
     
-    # IMPLEMENT THIS METHOD
-    return acc
+    return train_data, train_label, validation_data, validation_label, test_data, test_label
 
-def learnOLERegression(X,y):
-    # Inputs:                                                         
-    # X = N x d 
-    # y = N x 1                                                               
-    # Output: 
-    # w = d x 1                                                                
-    # IMPLEMENT THIS METHOD                                                   
-    return w
-
-def learnRidgeERegression(X,y,lambd):
-    # Inputs:
-    # X = N x d                                                               
-    # y = N x 1 
-    # lambd = ridge parameter (scalar)
-    # Output:                                                                  
-    # w = d x 1                                                                
-
-    # IMPLEMENT THIS METHOD                                                   
-    return w
-
-def testOLERegression(w,Xtest,ytest):
-    # Inputs:
-    # w = d x 1
-    # Xtest = N x d
-    # ytest = X x 1
-    # Output:
-    # rmse
+def sigmoid(z):
+    return 1.0/(1.0 + np.exp(-z));
     
-    # IMPLEMENT THIS METHOD
-    return rmse
+def blrObjFunction(params, *args):
+    """
+    blrObjFunction computes 2-class Logistic Regression error function and
+    its gradient.
 
-def regressionObjVal(w, X, y, lambd):
+    Input:
+        initialWeights: the weight vector of size (D + 1) x 1 
+        train_data: the data matrix of size N x D
+        labeli: the label vector of size N x 1 where each entry can be either 0 or 1
+                representing the label of corresponding feature vector
 
-    # compute squared error (scalar) and gradient of squared error with respect
-    # to w (vector) for the given data X and y and the regularization parameter
-    # lambda                                                                  
-
-    # IMPLEMENT THIS METHOD                                             
+    Output: 
+        error: the scalar value of error function of 2-class logistic regression
+        error_grad: the vector of size (D+1) x 1 representing the gradient of
+                    error function
+    """
+    n_data = train_data.shape[0];
+    n_feature = train_data.shape[1];
+    error = 0;
+    error_grad = np.zeros((n_feature+1,1));
+    
+    ##################
+    # YOUR CODE HERE #
+    ##################
+    
     return error, error_grad
 
-def mapNonLinear(x,p):
-    # Inputs:                                                                  
-    # x - a single column vector (N x 1)                                       
-    # p - integer (>= 0)                                                       
-    # Outputs:                                                                 
-    # Xd - (N x (d+1))                                                         
-    # IMPLEMENT THIS METHOD
-    return Xd
+def blrPredict(W, data):
+    """
+     blrObjFunction predicts the label of data given the data and parameter W 
+     of Logistic Regression
+     
+     Input:
+         W: the matrix of weight of size (D + 1) x 10. Each column is the weight 
+         vector of a Logistic Regression classifier.
+         X: the data matrix of size N x D
+         
+     Output: 
+         label: vector of size N x 1 representing the predicted label of 
+         corresponding feature vector given in data matrix
 
-# Main script
+    """
+    label = np.zeros((data.shape[0],1));
+    
+    ##################
+    # YOUR CODE HERE #
+    ##################
 
-# Problem 1
-# load the sample data                                                                 
-X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'),encoding = 'latin1')            
-
-# LDA
-means,covmat = ldaLearn(X,y)
-ldaacc = ldaTest(means,covmat,Xtest,ytest)
-print('LDA Accuracy = '+str(ldaacc))
-# QDA
-means,covmats = qdaLearn(X,y)
-qdaacc = qdaTest(means,covmats,Xtest,ytest)
-print('QDA Accuracy = '+str(qdaacc))
-
-# Problem 2
-
-X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'),encoding = 'latin1')   
-# add intercept
-X_i = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
-Xtest_i = np.concatenate((np.ones((Xtest.shape[0],1)), Xtest), axis=1)
-
-w = learnOLERegression(X,y)
-mle = testOLERegression(w,Xtest,ytest)
-
-w_i = learnOLERegression(X_i,y)
-mle_i = testOLERegression(w_i,Xtest_i,ytest)
-
-print('RMSE without intercept '+str(mle))
-print('RMSE with intercept '+str(mle_i))
-
-# Problem 3
-k = 101
-lambdas = np.linspace(0, 0.004, num=k)
-i = 0
-rmses3 = np.zeros((k,1))
-for lambd in lambdas:
-    w_l = learnRidgeRegression(X_i,y,lambd)
-    rmses3[i] = testOLERegression(w_l,Xtest_i,ytest)
-    i = i + 1
-plt.plot(lambdas,rmses3)
-
-# Problem 4
-k = 101
-lambdas = np.linspace(0, 0.004, num=k)
-i = 0
-rmses4 = np.zeros((k,1))
-opts = {'maxiter' : 100}    # Preferred value.                                                
-w_init = np.zeros((X_i.shape[1],1))
-for lambd in lambdas:
-    args = (X_i, y, lambd)
-    w_l = minimize(regressionObjVal, w_init, jac=True, args=args,method='CG', options=opts)
-    w_l_1 = np.zeros((X_i.shape[1],1))
-    for j in range(len(w_l.x)):
-        w_l_1[j] = w_l.x[j]
-    rmses4[i] = testOLERegression(w_l_1,Xtest_i,ytest)
-    i = i + 1
-plt.plot(lambdas,rmses4)
+    return label
 
 
-# Problem 5
-pmax = 7
-lambda_opt = lambdas[np.argmin(rmses4)]
-rmses5 = np.zeros((pmax,2))
-for p in range(pmax):
-    Xd = mapNonLinear(X[:,2],p)
-    Xdtest = mapNonLinear(Xtest[:,2],p)
-    w_d1 = learnRidgeRegression(Xd,y,0)
-    rmses5[p,0] = testOLERegression(w_d1,Xdtest,ytest)
-    w_d2 = learnRidgeRegression(Xd,y,lamda_opt)
-    rmses5[p,1] = testOLERegression(w_d2,Xdtest,ytest)
-plt.plot(range(pmax),rmses5)
-plt.legend(('No Regularization','Regularization'))
+"""
+Script for Logistic Regression
+"""
+train_data, train_label, validation_data,validation_label, test_data, test_label = preprocess();
+
+# number of classes
+n_class = 10;
+
+# number of training samples
+n_train = train_data.shape[0];
+
+# number of features
+n_feature = train_data.shape[1];
+
+T = np.zeros((n_train, n_class));
+for i in range(n_class):
+    T[:,i] = (train_label == i).astype(int).ravel();
+    
+# Logistic Regression with Gradient Descent
+W = np.zeros((n_feature+1, n_class));
+initialWeights = np.zeros((n_feature+1,1));
+opts = {'maxiter' : 50};
+for i in range(n_class):
+    labeli = T[:,i].reshape(n_train,1);
+    args = (train_data, labeli);
+    nn_params = minimize(blrObjFunction, initialWeights, jac=True, args=args,method='CG', options=opts)
+    W[:,i] = nn_params.x.reshape((n_feature+1,));
+
+# Find the accuracy on Training Dataset
+predicted_label = blrPredict(W, train_data);
+print('\n Training set Accuracy:' + str(100*np.mean((predicted_label == train_label).astype(float))) + '%')
+
+# Find the accuracy on Validation Dataset
+predicted_label = blrPredict(W, validation_data);
+print('\n Validation set Accuracy:' + str(100*np.mean((predicted_label == validation_label).astype(float))) + '%')
+
+# Find the accuracy on Testing Dataset
+predicted_label = blrPredict(W, test_data);
+print('\n Testing set Accuracy:' + str(100*np.mean((predicted_label == test_label).astype(float))) + '%')
+
+"""
+Script for Support Vector Machine
+"""
+
+print('\n\n--------------SVM-------------------\n\n')
+##################
+# YOUR CODE HERE #
+##################
+
